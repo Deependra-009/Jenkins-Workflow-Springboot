089208130ffd27d468bdf6c7c2759233b3eee414
Deependra-009 <deependra04trivedi@gmail.com>
2025-03-23 16:48:03 +0530

    test

--- src/main/java/com/jenkins/Jenkins_Workflow/JenkinsWorkflowApplication.java
+++ src/main/java/com/jenkins/Jenkins_Workflow/JenkinsWorkflowApplication.java
@@ -10,6 +10,18 @@ public class JenkinsWorkflowApplication {
 		SpringApplication.run(JenkinsWorkflowApplication.class, args);
 	}
 
+	/*
+
+📌 Steps in This Workflow
+✅ Runs on push and pull request events
+✅ Checks out the code
+✅ Sets up Java & Maven
+✅ Builds the project (mvn clean package)
+✅ Runs Unit & Integration Tests (mvn test)
+✅ Performs Static Code Analysis (Checkstyle + OWASP Security Scan)
+✅ Blocks Merge if tests fail
+
+	* */
 	public void Test(){
 		System.out.println("CI/CD Pipeline Example");
 	}
