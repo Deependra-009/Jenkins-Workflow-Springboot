8b7f1a8a7d9cb0288105db12577a30072f93ac00
Deependra-009 <deependra04trivedi@gmail.com>
2025-03-23 16:07:38 +0530

    Jenkins

---
+++ src/main/java/com/jenkins/Jenkins_Workflow/JenkinsWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.Jenkins_Workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsWorkflowApplication.class, args);
+	}
+
+}
